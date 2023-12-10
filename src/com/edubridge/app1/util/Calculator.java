97a41321211c869cd0b28e60a0c12f95dd10f049
GantasalaDevika <153358619+GantasalaDevika@users.noreply.github.com>
2023-12-10 22:23:15 +0530

    Update Calculator.java

--- src/com/edubridge/app1/util/Calculator.java
+++ src/com/edubridge/app1/util/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 	public int findSquare(int num) {
 		return num*num;
 	}
+	public int findCube(int num){
+		return num*num*num;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
