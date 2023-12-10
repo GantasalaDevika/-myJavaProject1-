064d408bddd5764ebffc6b954d0b98bcb84bb636
GantasalaDevika <devika001.g@gmail.com>
2023-12-10 22:07:12 +0530

    my first commit

---
+++ src/com/edubridge/app1/util/Calculator.java
@@ -0,0 +1,13 @@
+package com.edubridge.app1.util;
+
+public class Calculator {
+	public int findSquare(int num) {
+		return num*num;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
